836c7b02ed4989363e38b80d9afede0989df92f9
pinkvomittt <pink.vomittt@gmail.com>
2025-06-13 19:14:02 -0500

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,9 +1,3 @@
-export const config = {
-  api: {
-    bodyParser: true,
-  },
-};
-
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
@@ -19,7 +13,7 @@ export default async function handler(req, res) {
     const response = await fetch("https://legghxaprgxcxbskvhgh.supabase.co/auth/v1/token?grant_type=password", {
       method: "POST",
       headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxlZ2doeGFwcmd4Y3hic2t2aGdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyNjQ2NzksImV4cCI6MjA2NDg0MDY3OX0.d8T2APt1hvpgiFS4l_z0_LAOo3--XKQ0y95s_XxSaLw",
+        apikey: "YOUR_SUPABASE_API_KEY",
         "Content-Type": "application/json"
       },
       body: JSON.stringify({ email, password })
